0303c1c3bd91f2ec587cab4f5ab687378f131de7
Masked <trist13579@gmail.com>
2024-01-01 21:55:41 -0500

    Basic Setup

---
+++ rusty/target/debug/deps/rusty.d
@@ -0,0 +1,5 @@
+C:\Users\Masked\Documents\GitHub\Rusty\rusty\target\debug\deps\rusty.exe: src\main.rs
+
+C:\Users\Masked\Documents\GitHub\Rusty\rusty\target\debug\deps\rusty.d: src\main.rs
+
+src\main.rs:
---
+++ rusty/target/debug/rusty.d
@@ -0,0 +1,1 @@
+C:\Users\Masked\Documents\GitHub\Rusty\rusty\target\debug\rusty.exe: C:\Users\Masked\Documents\GitHub\Rusty\rusty\src\main.rs
